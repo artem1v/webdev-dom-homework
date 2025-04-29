08b36b5370fb0258fd399cd276a79a6271f2f2c1
John Doe <johndoe@email.com>
2025-04-29 17:22:09 +0500

    final

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 import { renderComments } from './render.js'
-import {
-    initAddCommentHandler,
-    initLikeHandlers,
-    initQuoteHandler,
-} from './handlers.js'
+import { initAddCommentListenner } from './handlers.js'
 
 document.addEventListener('DOMContentLoaded', () => {
     renderComments()
-    initAddCommentHandler()
-    initLikeHandlers()
-    initQuoteHandler()
+    initAddCommentListenner()
 })
